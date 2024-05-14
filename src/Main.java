b31265cb01850c79e382fc32f810e9a9d6aa5aeb
SoloMaksimilian <mephistosmad@gmail.com>
2024-05-14 18:32:42 +0300

    Ex2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-    for (int a=1; a<=10; a++){
+        for (int a = 1; a <= 10; a++) {
             System.out.println(a);
+        }
+
+        for (int b = 10; b >= 1; b--) {
+            System.out.println(b);
         }
     }
 }
